0c417a398f16dddb75004937a51a68c5125174c1
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2020-01-09 08:43:28 -0300

    Adding nlp NN

--- main.py
+++ main.py
@@ -28,9 +28,8 @@ net = ConvolutionalNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+
 # Training pass
-
-
 def train(trainloader, n_epochs=2):
     print('Training...')
     for epoch in range(n_epochs):
@@ -59,4 +58,49 @@ def train(trainloader, n_epochs=2):
     print('Model saved.')
 
 
-train(trainloader)
+train(trainloader, n_epochs=5)
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+net.load_state_dict(torch.load(MODEL_PATH))
+outputs = net(images)
+
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
